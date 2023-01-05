eb05582415081e87cfaff8e10d40315b116707b9
argo <argyyyn@gmail.com>
2023-01-06 00:28:38 +0600

    Setting TS up

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        arg
+      </header>
+    </div>
+  );
+}
+
+export default App;
